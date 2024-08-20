a4f87223aedcd61a6dacfc9f7450fb1419307330
muhalfatih <muhammadal.fatih9@gmail.com>
2024-08-20 07:19:11 +0700

    update cp. 83

--- tic-tac-toe-starting-project/src/App.jsx
+++ tic-tac-toe-starting-project/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
 
   function handleSelectSquare() {
     setActivePlayer(curActivePlayer => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns();
   }
 
   return (
@@ -20,6 +23,7 @@ function App() {
 
         <GameBoard onSelectSquare={handleSelectSquare} activeSymbolPlayer={activePlayer} />
       </div>
+      <Log />
     </main>
   )
 }
